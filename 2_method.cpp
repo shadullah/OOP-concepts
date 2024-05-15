069a8d9838a0ca81567ec49826318cb953894cf7
hexaalif <shadullahsad9@gmail.com>
2024-05-15 18:24:59 +0600

    steps method

--- 2_method.cpp
+++ 2_method.cpp
@@ -18,13 +18,22 @@ public:
         my_val = v;
         cout << "constructor called\n";
     }
+
+    int myOutsider(int v);
 };
+
+// outside method definition
+int Testing_obj::myOutsider(int v)
+{
+    return v;
+}
 
 int main()
 {
     Testing_obj obj(5);
     obj.res();
     obj.my_val = 120;
-    cout << obj.my_val;
+    cout << obj.my_val << endl;
+    cout << obj.myOutsider(35);
     return 0;
 }
